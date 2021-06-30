d45709626ff227895a519c94c6082fb85564f4ef
Kiana Long <Kianajade42.kl@gmail.com>
2021-06-30 12:08:43 -0400

    first commit

---
+++ lib/plantfacts.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "plantfacts/version"
+
+module Plantfacts
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/plantfacts/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Plantfacts
+  VERSION = "0.1.0"
+end
